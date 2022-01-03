8140a98be7ca899c9f7174a393de87b7839e282f
Samandar <samandarcoderdev@gmail.com>
2022-01-03 13:54:26 +0500

    Task1

---
+++ app/src/main/java/com/example/layoutskotlin/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.layoutskotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
